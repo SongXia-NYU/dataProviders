1d02784c07d9b42751616cdf4f4d413df227031e
Song <55320092+SongXia-NYU@users.noreply.github.com>
2021-03-01 08:46:01 -0500

    Update DummyIMDataset.py

--- DummyIMDataset.py
+++ DummyIMDataset.py
@@ -3,11 +3,19 @@ from torch_geometric.data import InMemoryDataset
 
 
 class DummyIMDataset(InMemoryDataset):
-    def __init__(self, root, dataset_name):
+    def __init__(self, root, dataset_name, split=None, **kwargs):
         self.dataset_name = dataset_name
+        self.split = split
         super().__init__(root, None, None)
         self.data, self.slices = torch.load(self.processed_paths[0])
         self.train_index, self.val_index, self.test_index = None, None, None
+        if split is not None:
+            split_data = torch.load(self.processed_paths[1])
+            self.train_index = split_data["train_index"]
+            # I dont know why I used "valid_index" in disc and "val_index" in memory, but it takes a lot of effort to
+            # correct it now.
+            self.val_index = split_data["valid_index"]
+            self.test_index = split_data["test_index"]
 
     @property
     def raw_file_names(self):
@@ -15,7 +23,7 @@ class DummyIMDataset(InMemoryDataset):
 
     @property
     def processed_file_names(self):
-        return [self.dataset_name]
+        return [self.dataset_name, self.split]
 
     def download(self):
         pass
