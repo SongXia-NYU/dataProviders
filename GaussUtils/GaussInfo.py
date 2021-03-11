9ab09c933426b9618af8ea9e8e2d5c70ad67d98e
Song <55320092+SongXia-NYU@users.noreply.github.com>
2021-03-11 15:52:19 -0500

    Update GaussInfo.py

--- GaussUtils/GaussInfo.py
+++ GaussUtils/GaussInfo.py
@@ -240,7 +240,7 @@ def read_gauss_log(input_file, output_path, indexes, gauss_version):
 
 
 def preprocess_frag20_sol():
-    geometry = "qm"
+    geometry = "mmff"
     dd_csv_folder = "/scratch/projects/yzlab/group/temp_dd/solvation/calculated/"
     train_csv = pd.read_csv(osp.join(dd_csv_folder, "train.csv"))
     valid_csv = pd.read_csv(osp.join(dd_csv_folder, "valid.csv"))
