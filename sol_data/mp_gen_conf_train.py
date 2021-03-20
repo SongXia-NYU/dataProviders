66159ec831feaed6f2bea39b43c1c46f4ebd83fd
Song <55320092+SongXia-NYU@users.noreply.github.com>
2021-03-20 07:32:38 -0400

    new mp

---
+++ sol_data/mp_gen_conf_train.py
@@ -0,0 +1,23 @@
+from multiprocessing import Pool
+
+from DataGen.genconfs import runGenerator
+import pandas as pd
+import os.path as osp
+import torch
+
+
+def _run_generator(i):
+
+    runGenerator(i, concat_csv["SMILES"].tolist()[i], "sol", "/scratch/sx801/data/sol-frag20-ccdc/mmff_confs/")
+
+
+if __name__ == '__main__':
+    dd_csv_folder = "/scratch/projects/yzlab/group/temp_dd/solvation/calculated/"
+    train_csv = pd.read_csv(osp.join(dd_csv_folder, "train.csv"))
+    valid_csv = pd.read_csv(osp.join(dd_csv_folder, "valid.csv"))
+    test_csv = pd.read_csv(osp.join(dd_csv_folder, "test.csv"))
+    # concatenate them in this order
+    error_list = torch.load("conf_error_list.pt")[160:]
+    concat_csv = pd.concat([train_csv, valid_csv, test_csv], ignore_index=True).iloc[error_list]
+    with Pool(20) as p:
+        p.map(_run_generator, concat_csv.index.tolist())
