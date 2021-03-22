e874298725d25e7cd693af18029772346ca902d1
Song <55320092+SongXia-NYU@users.noreply.github.com>
2021-03-22 16:13:19 -0400

    Update tmp.generate_sol_mmff_split.py

--- tmp.generate_sol_mmff_split.py
+++ tmp.generate_sol_mmff_split.py
@@ -9,24 +9,13 @@ if __name__ == '__main__':
     valid_csv = pd.read_csv(osp.join(dd_csv_folder, "valid.csv"))
     test_csv = pd.read_csv(osp.join(dd_csv_folder, "test.csv"))
 
-    train_index = []
-    valid_index = []
-    test_index = []
-
     success_map = torch.load("success_map.pt")
-    train_map = torch.zeros_like(success_map)
-    valid_map = torch.zeros_like(success_map)
-    test_map = torch.zeros_like(success_map)
-    train_map[:train_csv.shape[0]] = 1
-    valid_map[train_csv.shape[0]:train_csv.shape[0]+valid_csv.shape[0]] = 1
-    test_map[-test_csv.shape[0]:] = 1
-
-    train_mask = train_map & success_map
-    valid_mask = valid_map & success_map
-    test_mask = test_map & success_map
+    train_size = success_map[:train_csv.shape[0]].sum()
+    valid_size = success_map[train_csv.shape[0]:train_csv.shape[0]+valid_csv.shape[0]].sum()
+    test_size = success_map[-test_csv.shape[0]:].sum()
 
     save_root = "data/processed"
-    torch.save({"train_index": train_mask.nonzero().reshape(-1),
-                "valid_index": valid_mask.nonzero().reshape(-1),
-                "test_index": test_mask.nonzero().reshape(-1)},
+    torch.save({"train_index": torch.arange(train_size),
+                "valid_index": torch.arange(train_size, train_size+valid_size),
+                "test_index": torch.arange(train_size+valid_size, train_size+valid_size+test_size)},
                osp.join(save_root, "frag20_sol_split_mmff_gen_03222021.pt"))
