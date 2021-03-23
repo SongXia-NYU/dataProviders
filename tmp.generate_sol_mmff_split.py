ebf7c6a171257a989d9c120e988d54ac974cf269
Song <55320092+SongXia-NYU@users.noreply.github.com>
2021-03-23 14:18:54 -0400

    Update tmp.generate_sol_mmff_split.py

--- tmp.generate_sol_mmff_split.py
+++ tmp.generate_sol_mmff_split.py
@@ -2,7 +2,6 @@ import pandas as pd
 import os.path as osp
 import torch
 
-
 if __name__ == '__main__':
     dd_csv_folder = "/scratch/projects/yzlab/group/temp_dd/solvation/calculated/"
     train_csv = pd.read_csv(osp.join(dd_csv_folder, "train.csv"))
@@ -11,16 +10,18 @@ if __name__ == '__main__':
 
     success_map = torch.load("success_map.pt")
     train_size = success_map[:train_csv.shape[0]].sum()
-    valid_size = success_map[train_csv.shape[0]:train_csv.shape[0]+valid_csv.shape[0]].sum()
+    valid_size = success_map[train_csv.shape[0]:train_csv.shape[0] + valid_csv.shape[0]].sum()
     test_size = success_map[-test_csv.shape[0]:].sum()
 
     duplicate_map = torch.load("sol_data/inchi_exist_map.pt")
     dup_to_suc_map = duplicate_map[torch.nonzero(success_map).view(-1)]
 
     save_root = "data/processed"
-    torch.save({"train_index": torch.arange(train_size)[dup_to_suc_map[:train_size]],
+    torch.save({"train_index": torch.arange(train_size)[dup_to_suc_map[:train_size] == 0],
                 "valid_index":
-                    torch.arange(train_size, train_size+valid_size)[dup_to_suc_map[train_size: train_size+valid_size]],
+                    torch.arange(train_size, train_size + valid_size)[
+                        dup_to_suc_map[train_size: train_size + valid_size] == 0],
                 "test_index":
-                    torch.arange(train_size+valid_size, train_size+valid_size+test_size)[dup_to_suc_map[-test_size:]]},
+                    torch.arange(train_size + valid_size, train_size + valid_size + test_size)[
+                        dup_to_suc_map[-test_size:] == 0]},
                osp.join(save_root, "frag20_sol_split_mmff_gen_03222021.pt"))
