b200a2a898642c6e95182ab5d8e54306f5073378
Song <55320092+SongXia-NYU@users.noreply.github.com>
2021-06-28 16:45:59 -0400

    Modify subjob

--- sol_data/sub_jobs.py
+++ sol_data/sub_jobs.py
@@ -2,6 +2,9 @@ from glob import glob
 import os
 
 if __name__ == '__main__':
-    files = glob("raw/jobs/job*.pbs")
-    for f in files:
-        os.system("sbatch {}".format(f))
+    start = 0
+    end = 800
+    files = "raw/openchem_logP_logs/job_{}.sbatch"
+    for i in range(start, end):
+        f_name = files.format(i)
+        os.system("sbatch {}".format(f_name))
