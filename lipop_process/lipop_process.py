88aeb10cec5ad78c537a523f528e34ea0c56be8c
Song Xia <sx801@nyu.edu>
2021-09-16 16:53:34 -0400

    fix a conflict, csd sdf to coms

--- lipop_process/lipop_process.py
+++ lipop_process/lipop_process.py
@@ -6,13 +6,8 @@ from GaussUtils.combine_3phases_data import infuse_energy, combine_csv
 
 if __name__ == '__main__':
     # read_gauss_log("../sol_data/raw/lipop_logs/*.log", "lipop")
-<<<<<<< HEAD
     # read_gauss_log("../sol_data/raw/lipop_water_coms/*.log", "lipop_water")
     read_gauss_log("../sol_data/raw/lipop_oct_coms/*.log", "lipop_oct")
-=======
-    # read_gauss_log("../sol_data/raw/lipop_water_coms/*.log", "lipop_water", test_run=False)
-    # read_gauss_log("../sol_data/raw/lipop_oct_coms/*.log", "lipop_oct")
->>>>>>> 7c954564772de1823fa5efc0a919157a7bcc3acb
     # combine_csv("lipop.csv", "lipop_water.csv", "lipop_oct.csv", "lipop_paper.csv", "lipop_sol.csv")
     # os.makedirs("./processed", exist_ok=True)
     # shutil.move("lipop.pt", "processed/lipop.pt")
