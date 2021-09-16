eb7bea2cd84f4153d1dd95a165c74d96d97cc655
Song Xia <sx801@log-2.nyu.cluster>
2021-09-16 15:34:11 -0400

    updarte

--- lipop_process/lipop_process.py
+++ lipop_process/lipop_process.py
@@ -2,9 +2,9 @@ from GaussUtils.GaussInfo import read_gauss_log
 from GaussUtils.combine_3phases_data import infuse_energy
 
 if __name__ == '__main__':
-    read_gauss_log("../sol_data/raw/lipop_logs/*.log", "lipop")
-    # read_gauss_log("../sol_data/raw/lipop_water_logs/*.log", "lipop_water")
-    # read_gauss_log("../sol_data/raw/lipop_oct_logs/*.log", "lipop_oct")
+    # read_gauss_log("../sol_data/raw/lipop_logs/*.log", "lipop")
+    # read_gauss_log("../sol_data/raw/lipop_water_coms/*.log", "lipop_water")
+    read_gauss_log("../sol_data/raw/lipop_oct_coms/*.log", "lipop_oct")
     # combine_csv("lipop.csv", "lipop_water.csv", "lipop_oct.csv", "lipop_paper.csv", "lipop_sol.csv")
     # os.makedirs("./processed", exist_ok=True)
     # shutil.move("lipop.pt", "processed/lipop.pt")
--- sol_data/sub_jobs.py
+++ sol_data/sub_jobs.py
@@ -2,9 +2,9 @@ from glob import glob
 import os
 
 if __name__ == '__main__':
-    start = 2400
-    end = 3000
-    files = "raw/openchem_logP_logs/job_{}.sbatch"
+    start = 0
+    end = 641
+    files = "raw/freesolv_oct_logs/job_{}.sbatch"
     for i in range(start, end):
         f_name = files.format(i)
         os.system("sbatch {}".format(f_name))
