ce7771eb62057ac6124a24636fa26ddc059b4c4d
Song <55320092+SongXia-NYU@users.noreply.github.com>
2021-11-10 16:50:16 -0500

    Delete local files

--- bbbp_process/bbbp_process.py
+++
@@ -1,4 +0,0 @@
-from GaussUtils.GaussInfo import sdf_to_pt_custom
-
-if __name__ == '__main__':
-    sdf_to_pt_custom("../sol_data/BBBP.csv", None, "../sol_data/raw/BBBP_mmff_sdfs", ".", "BBBP", "mmff")
