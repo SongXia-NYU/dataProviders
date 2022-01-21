840b280429861bae56d8bef71feb19ae36608435
Song Xia <sx801@log-2.hpc.nyu.edu>
2022-01-21 15:24:12 -0500

    update

--- scripts/add_acsf.py
+++ scripts/add_acsf.py
@@ -10,8 +10,8 @@ from tqdm import tqdm
 
 
 def add_acsf():
-    dataset_name = "freesolv_mmff_pyg.pt"
-    save_name = "freesolv_mmff_acsf308_pyg.pt"
+    dataset_name = "frag20reducedAllSolRef-Bmsg-cutoff-10.00-sorted-defined_edge-lr-MMFF.pt"
+    save_name = "frag20reducedAllSolRef-Bmsg-cutoff-10.00-sorted-defined_edge-lr-acsf308-MMFF.pt"
 
     acsf = ACSF(species=['B', 'Br', 'C', 'Cl', 'F', 'H', 'N', 'O', 'P', 'S', 'I'], rcut=6.0,
                 g2_params=[[1, 1], [1, 2], [1, 3]], g4_params=[[1, 1, 1], [1, 2, 1], [1, 1, -1], [1, 2, -1]])
@@ -22,7 +22,7 @@ def add_acsf():
     for i in tqdm(range(len(dataset)), total=len(dataset)):
         this_data = dataset[i]
         atoms = Atoms(numbers=this_data.Z.numpy(), positions=this_data.R.numpy())
-        this_acsf = acsf.create(atoms)
+        this_acsf = acsf.create(atoms, n_jobs=4)
         this_data.acsf = this_acsf
 
         data_list.append(this_data)
