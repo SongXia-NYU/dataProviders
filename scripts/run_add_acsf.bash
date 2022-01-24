02ffe202480696e6c865c2f979ac541b1ad9615c
Song Xia <sx801@log-2.hpc.nyu.edu>
2022-01-24 16:24:45 -0500

    udpate

--- scripts/run_add_acsf.bash
+++ scripts/run_add_acsf.bash
@@ -9,5 +9,5 @@
 module purge
 singularity exec --nv --overlay ~/conda_envs/pytorch1.8.1-rocm4.0.1-extra-5GB-3.2M.ext3:ro --overlay ~/conda_envs/pytorch1.8.1-rocm4.0.1.sqf:ro \
        /scratch/work/public/hudson/images/rocm-4.0.1.sif bash -c \
-       "source /ext3/env.sh; export PYTHONPATH=..:$PYTHONPATH; python add_acsf.py --dataset_name frag20reducedAllSolRef-Bmsg-cutoff-10.00-sorted-defined_edge-lr-QM.pt \
-       --save_name frag20reducedAllSolRef-Bmsg-cutoff-10.00-sorted-defined_edge-lr-acsf308-QM.pt"
+       "source /ext3/env.sh; export PYTHONPATH=..:$PYTHONPATH; python add_acsf.py --dataset_name frag20reducedAllSolRef-Bmsg-cutoff-10.00-sorted-defined_edge-lr-MMFF.pt \
+       --save_name frag20reducedAllSolRef-Bmsg-cutoff-10.00-sorted-defined_edge-lr-acsf308-MMFF.pt"
